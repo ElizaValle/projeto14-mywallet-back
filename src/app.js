53064ae7e16c7683f0e2fd32eae0c6fb0cd3e87a
ElizaValle <beteduvalle@gmail.com>
2023-08-23 17:10:12 -0300

    requisito POST /sign-up sem tratamento de senha

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import joi from "joi"
 
 const app = express()
 app.use(express.json())
@@ -17,6 +18,34 @@ try {
     console.log(err.message)
 }
 const db = mongoClient.db()
+
+// schemas
+const schemaCadastro = joi.object({
+    nome: joi.string().required(),
+    email: joi.string().email().required(),
+    senha: joi.string().min(3).required(),
+    confirmeSenha: joi.string().min(3).required()
+})
+
+// endpoints
+app.post("/sign-up", async (req, res) => {
+    const { nome, email, senha, confirmeSenha } = req.body
+
+    const validacao = schemaCadastro.validate(req.body, { abortEarly: false })
+    if (validacao.error) return res.sendStatus(422)
+
+    try {
+        const emailUsuario = await db.collection("usuarios").findOne({ email })
+        if (emailUsuario) return res.sendStatus(409)
+
+        await db.collection("usuarios").insertOne({ nome, email, senha })
+
+        res.sendStatus(201)
+
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+})
 
 // escuta da porta
 const PORT = 5000
