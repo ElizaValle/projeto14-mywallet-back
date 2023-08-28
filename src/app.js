7da34b2c40ad3118b2393008b1ccc49b56ee3838
ElizaValle <beteduvalle@gmail.com>
2023-08-28 17:43:03 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ app.use(cors())
 app.use(authRouter)
 app.use(transactionRouter)
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
